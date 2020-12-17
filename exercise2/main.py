05e2fc49050f3b4210bb99a3e09faf89a1ed3bab
giorge1 <g.ammendola4@studenti.unisa.it>
2020-12-17 13:01:50 +0100

    added in-code doc and improved memory usage in some scenarios
    Co-authored-by: r4004 <r.imparato13@studenti.unisa.it>

--- exercise2/main.py
+++ exercise2/main.py
@@ -1,13 +1,45 @@
 from data_structures.currency import Currency
+import decimal
 
 
-def denominations_combinations(cur: Currency, r: float):
-    amount = int(round(r, 2) * 100)
+def get_decimal_places(f):
+    """
+    Method used to get the number of the meaningful decimal places of the parameter.
+    :param f: the float value
+    :return: the number of the meaningful decimal places of the parameter
+    """
+    return -decimal.Decimal(str(f)).as_tuple().exponent
+
+
+def denominations_combinations(cur: Currency, amount: float):
+    """
+    This function returns the number of different ways that value given as parameter can be achieved
+    by using all the possible comibinations of the denominations of the given currency.
+    :param cur: currency to use
+    :param amount: the total amount to be achieved
+    :return: the number of all the possible cominations of denominations that match the amount
+    """
+    amount = round(amount, 2)
+    amount_decimal_places = get_decimal_places(amount)
+
+    den_decimal_places = 0
     temp_den = []
     for den in cur.iter_denominations():
-        temp_den.append(int(round(den, 2) * 100))
+        if den <= amount:
+            den_decimal_places = max(den_decimal_places, get_decimal_places(round(den, 2)))
+            temp_den.append(round(den, 2))
 
-    sol = [[0]*(amount+1) for _ in range(len(temp_den))]
+    if amount_decimal_places > den_decimal_places:
+        # if the smallest denomination has less decimal places than the amount, then a solution does not exist
+        return 0
+
+    # the amount and the denominations are multiplied by the minimum possible value
+    # e.g. if amount = 2.20 and den = [0.10, 0.20, 0.50] then everything is multiplied by 10^1
+    amount = int(round(amount, 2) * (10 ** den_decimal_places))
+    temp_den = [int(e * (10 ** den_decimal_places)) for e in temp_den]
+
+    # dynamic programmic solution
+    sol = [[0] * (amount + 1) for _ in range(len(temp_den))]
     for i in range(len(temp_den)):
         for j in range(amount + 1):
             if j == 0:
@@ -17,9 +49,9 @@ def denominations_combinations(cur: Currency, r: float):
                 sol[i][j] = 1 if j % temp_den[i] == 0 else 0
                 continue
             if j >= temp_den[i]:
-                sol[i][j] = sol[i-1][j] + sol[i][j - temp_den[i]]
+                sol[i][j] = sol[i - 1][j] + sol[i][j - temp_den[i]]
             else:
-                sol[i][j] = sol[i-1][j]
+                sol[i][j] = sol[i - 1][j]
     return sol[-1][-1]
 
 
@@ -40,7 +72,7 @@ def get_currency(c="EUR", d=None):
 
 def main():
     c = get_currency()
-    print(denominations_combinations(c, 10))
+    print(denominations_combinations(c, 0.10))
 
 
 if __name__ == '__main__':
